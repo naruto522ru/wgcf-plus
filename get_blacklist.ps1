9369d5714b7264dcd53b7d3eb07ba09d0302c543
naruto522ru <itachi522ru@gmail.com>
2022-07-06 03:01:13 +0300

    Fix type

--- get_blacklist.ps1
+++ get_blacklist.ps1
@@ -1,2 +1,2 @@
 Import-Module -Name BitsTransfer
-Start-BitsTransfer -Source https://antizapret.prostovpn.org/domains-export.txt .\russia-blacklist.txtk
+Start-BitsTransfer -Source https://antizapret.prostovpn.org/domains-export.txt .\russia-blacklist.txt
