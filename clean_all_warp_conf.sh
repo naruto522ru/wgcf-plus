6ed6fb25831c72b3d7df4e9afeed0c732afccf58
naruto522ru <itachi522ru@gmail.com>
2022-07-23 14:01:15 +0300

    Fix shebang for portability

--- clean_all_warp_conf.sh
+++ clean_all_warp_conf.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 find ./ -type f -name "*.conf" -delete
 find ./ -type f -name "*.toml" -delete
 exit 0
--- get_blacklist.sh
+++ get_blacklist.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [[ $(command -v curl) ]]; then
 curl -# -64 https://antizapret.prostovpn.org/domains-export.txt > ./russia-blacklist.txt;
 else
