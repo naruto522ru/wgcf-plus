c8d8e1e8c5fc0abe00f664d095d62c1dc05df0e1
IA <itachi522ru@gmail.com>
2023-04-23 23:41:05 +0300

    Update get_blacklist.sh

--- get_blacklist.sh
+++ get_blacklist.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ $(command -v curl) ]]; then
-curl -# -64 https://antizapret.prostovpn.org/domains-export.txt > ./russia-blacklist.txt;
+curl -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0' -# -64 https://antizapret.prostovpn.org/domains-export.txt > ./russia-blacklist.txt;
 else
 if [[ $(command -v wget) ]]; then
 wget -4 -t 3 https://antizapret.prostovpn.org/domains-export.txt -O - > ./russia-blacklist.txt;
