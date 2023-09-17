5d1c505a827ffc03eb45940326991d5c62f9fc6f
IA <itachi522ru@gmail.com>
2023-09-18 01:16:39 +0300

    Edit get_blacklist.ps1

--- get_blacklist.ps1
+++ get_blacklist.ps1
@@ -1,2 +1,13 @@
-Import-Module -Name BitsTransfer
-Start-BitsTransfer -Source https://antizapret.prostovpn.org/domains-export.txt .\russia-blacklist.txt
+$sourceUrl = "https://antizapret.prostovpn.org/domains-export.txt"
+$destinationPath = "$PWD\russia-blacklist.txt"
+$userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0"
+
+$webClient = New-Object System.Net.WebClient
+$webClient.Headers.Add("User-Agent", $userAgent)
+
+try {
+    $webClient.DownloadFile($sourceUrl, $destinationPath)
+    Write-Host "File downloaded successfully in $destinationPath."
+} catch {
+    Write-Host "Error downloading file: $_"
+}
